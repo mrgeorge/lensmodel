d6a8ee555dbaf829b795f0227db0162ab76bf403
Matt George <mattgeorge1@gmail.com>
2013-07-30 18:15:02 +0000

    just added plotting preface for latex to plot.py

--- lensmodel/plot.py
+++ lensmodel/plot.py
@@ -1,3 +1,7 @@
 #! env python
 
 import matplotlib.pyplot as plt
+
+plt.rc('font',**{'family':'sans-serif','sans-serif':['Helvetica'],'size':20})
+plt.rc('text', usetex=True)
+plt.rc('axes',linewidth=1.5)
