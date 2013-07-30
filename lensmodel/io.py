99d2902e921c5eeb4ae2a85f22a80d92794afcc0
Matt George <mattgeorge1@gmail.com>
2013-07-30 00:09:38 +0000

    added readData to read Rachel's 3-column ascii data file

--- lensmodel/io.py
+++ lensmodel/io.py
@@ -1,9 +1,13 @@
 #! env python
 
-import fitsio
-import astropy.io.ascii
+import numpy as np
 
-def readData():
-    """Read Rachel's data files."""
-    pass
-
+def readData(filename):
+    """Read Rachel's data files.
+    Returns:
+       xshear - comoving Mpc/h
+       yshear - delta sigma in h Msun / comoving pc **2
+       errshear - error in delta sigma
+    """
+    xshear,yshear,errshear=np.loadtxt(filename,unpack=True)
+    return (xshear,yshear,errshear)
