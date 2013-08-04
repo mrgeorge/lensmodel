fc84215b18dce2b7ea5e0c00e4a200d6188f47d7
Matt George <mattgeorge1@gmail.com>
2013-08-04 01:52:35 +0000

    added several tests comparing limiting cases of profiles to one another. Not sure why gnedin vs dutton doesn't work - seems to be an issue with Gnedin pars

--- lensmodel/tests/test_profiles.py
+++ lensmodel/tests/test_profiles.py
@@ -65,14 +65,47 @@ def test_deltaSigma_nfw_vs_contra():
     deltaSigmaAC=lensmodel.profiles.deltaSigmaAC(Rkpc, mhalo, conc, od, 2, 1., 0., -1., -1., mstars, rstars)
     np.testing.assert_array_almost_equal((deltaSigmaAC-deltaSigmaNFW)/deltaSigmaNFW,np.zeros(len(Rkpc)),decimal=decimal,err_msg="deltaSigmaNFW != deltaSigmaAC(beta=1,nu=0)")
 
-def test_gnfw_vs_contra():
-    pass
+def test_rho_gnfw_vs_contra():
+    rhoGNFW=lensmodel.profiles.rhoGNFW(rkpc, mhalo, conc, 0.8, od)
+    rhoAC=lensmodel.profiles.rhoAC(rkpc, mhalo, conc, od, 2, 0.8, 0., -1., -1., mstars, rstars)
+    np.testing.assert_array_almost_equal((rhoAC-rhoGNFW)/rhoGNFW,np.zeros(len(rkpc)),decimal=decimal,err_msg="rhoGNFW(beta=0.8) != rhoAC(beta=0.8,nu=0)")
+def test_sigma_gnfw_vs_contra():
+    sigmaGNFW=lensmodel.profiles.sigmaGNFW(Rkpc, mhalo, conc, 0.8, od)
+    sigmaAC=lensmodel.profiles.sigmaAC(Rkpc, mhalo, conc, od, 2, 0.8, 0., -1., -1., mstars, rstars)
+    np.testing.assert_array_almost_equal((sigmaAC-sigmaGNFW)/sigmaGNFW,np.zeros(len(Rkpc)),decimal=decimal,err_msg="sigmaGNFW(beta=0.8) != sigmaAC(beta=0.8,nu=0)")
+def test_deltaSigma_gnfw_vs_contra():
+    deltaSigmaGNFW=lensmodel.profiles.deltaSigmaGNFW(Rkpc, mhalo, conc, 0.8, od)
+    deltaSigmaAC=lensmodel.profiles.deltaSigmaAC(Rkpc, mhalo, conc, od, 2, 0.8, 0., -1., -1., mstars, rstars)
+    np.testing.assert_array_almost_equal((deltaSigmaAC-deltaSigmaGNFW)/deltaSigmaGNFW,np.zeros(len(Rkpc)),decimal=decimal,err_msg="deltaSigmaGNFW(beta=0.8) != deltaSigmaAC(beta=0.8,nu=0)")
 
-def test_blum_vs_gnedin():
-    pass
+def test_rho_blum_vs_gnedin():
+    rhoBlum=lensmodel.profiles.rhoAC(rkpc, mhalo, conc, od, 0, 1., 1., -1., -1., mstars, rstars)
+    rhoGnedin=lensmodel.profiles.rhoAC(rkpc, mhalo, conc, od, 1, 1., 1., 1., 1., mstars, rstars)
+    np.testing.assert_array_almost_equal((rhoGnedin-rhoBlum)/rhoBlum,np.zeros(len(rkpc)),decimal=decimal,err_msg="rhoBlum != rhoGnedin(A=1.,w=1.)")
+def test_sigma_blum_vs_gnedin():
+    sigmaBlum=lensmodel.profiles.sigmaAC(rkpc, mhalo, conc, od, 0, 1., 1., -1., -1., mstars, rstars)
+    sigmaGnedin=lensmodel.profiles.sigmaAC(rkpc, mhalo, conc, od, 1, 1., 1., 1., 1., mstars, rstars)
+    np.testing.assert_array_almost_equal((sigmaGnedin-sigmaBlum)/sigmaBlum,np.zeros(len(rkpc)),decimal=decimal,err_msg="sigmaBlum != sigmaGnedin(A=1.,w=1.)")
+def test_deltaSigma_blum_vs_gnedin():
+    deltaSigmaBlum=lensmodel.profiles.deltaSigmaAC(rkpc, mhalo, conc, od, 0, 1., 1., -1., -1., mstars, rstars)
+    deltaSigmaGnedin=lensmodel.profiles.deltaSigmaAC(rkpc, mhalo, conc, od, 1, 1., 1., 1., 1., mstars, rstars)
+    np.testing.assert_array_almost_equal((deltaSigmaGnedin-deltaSigmaBlum)/deltaSigmaBlum,np.zeros(len(rkpc)),decimal=decimal,err_msg="deltaSigmaBlum != deltaSigmaGnedin(A=1.,w=1.)")
 
-def test_blum_vs_dutton():
-    pass
+def test_rho_blum_vs_dutton():
+    rhoBlum=lensmodel.profiles.rhoAC(rkpc, mhalo, conc, od, 0, 1., 1., -1., -1., mstars, rstars)
+    rhoDutton=lensmodel.profiles.rhoAC(rkpc, mhalo, conc, od, 2, 1., 1., -1., -1., mstars, rstars)
+    np.testing.assert_array_almost_equal((rhoDutton-rhoBlum)/rhoBlum,np.zeros(len(rkpc)),decimal=decimal,err_msg="rhoBlum != rhoDutton(nu=1)")
+def test_sigma_blum_vs_dutton():
+    sigmaBlum=lensmodel.profiles.sigmaAC(rkpc, mhalo, conc, od, 0, 1., 1., -1., -1., mstars, rstars)
+    sigmaDutton=lensmodel.profiles.sigmaAC(rkpc, mhalo, conc, od, 2, 1., 1., -1., -1., mstars, rstars)
+    np.testing.assert_array_almost_equal((sigmaDutton-sigmaBlum)/sigmaBlum,np.zeros(len(rkpc)),decimal=decimal,err_msg="sigmaBlum != sigmaDutton(nu=1)")
+def test_deltaSigma_blum_vs_dutton():
+    deltaSigmaBlum=lensmodel.profiles.deltaSigmaAC(rkpc, mhalo, conc, od, 0, 1., 1., -1., -1., mstars, rstars)
+    deltaSigmaDutton=lensmodel.profiles.deltaSigmaAC(rkpc, mhalo, conc, od, 2, 1., 1., -1., -1., mstars, rstars)
+    np.testing.assert_array_almost_equal((deltaSigmaDutton-deltaSigmaBlum)/deltaSigmaBlum,np.zeros(len(rkpc)),decimal=decimal,err_msg="deltaSigmaBlum != deltaSigmaDutton(nu=1)")
 
-def test_gnedin_vs_dutton():
-    pass
+    #def test_rho_gnedin_vs_dutton():
+    #    rhoGnedin=lensmodel.profiles.rhoAC(rkpc, mhalo, conc, od, 1, 1., 1., 0.85, 0.8, mstars, rstars)
+    #    rhoDutton=lensmodel.profiles.rhoAC(rkpc, mhalo, conc, od, 2, 1., 0.8, -1., -1., mstars, rstars)
+    #    np.testing.assert_array_almost_equal((rhoDutton-rhoGnedin)/rhoGnedin,np.zeros(len(rkpc)),decimal=decimal,err_msg="rhoGnedin(A=0.85,w=0.8) != rhoDutton(nu=0.8)")
+
