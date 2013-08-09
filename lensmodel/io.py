99c6bc6bc2980b77d19e527a3f4f681eab1b7849
Matt George <mattgeorge1@gmail.com>
2013-08-09 18:24:11 +0000

    copied parsToRec, chainToRec, recToPars, writeRec, and readRec from sim.py, just changing the default label names. This will allow saving chains so plots can be remade afterward

--- lensmodel/io.py
+++ lensmodel/io.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import profiles
+import fitsio
 
 def readData(filename):
     """Read Rachel's data files.
@@ -26,3 +27,34 @@ def convertCosmo(xshear,yshear,errshear,redshift=0.1):
     errOut=errshear * h * (1.+redshift)**2
 
     return (xOut,yOut,errOut)
+
+def parsToRec(pars,labels=np.array(["logMstar", "logRstar", "logMhalo", "conc", "innerSlopeGNFW", "nuDutton", "AGnedin", "wGnedin"])):
+    dtype=[(label,float) for label in labels]
+    rec=np.recarray(len(pars),dtype=dtype)
+    for ii in range(len(labels)):
+        rec[labels[ii]]=pars[:,ii]
+    return rec
+
+def chainToRec(chain,lnprob,labels=np.array(["logMstar", "logRstar", "logMhalo", "conc", "innerSlopeGNFW", "nuDutton", "AGnedin", "wGnedin"])):
+    nGal=chain.shape[0]
+    nPars=chain.shape[1]
+    arr=np.zeros((nGal,nPars+1))
+    arr[:,:-1]=chain
+    arr[:,-1]=lnprob
+    labels=np.append(labels,"lnprob")
+    rec=parsToRec(arr,labels=labels)
+    return rec
+
+def recToPars(rec,labels=np.array(["logMstar", "logRstar", "logMhalo", "conc", "innerSlopeGNFW", "nuDutton", "AGnedin", "wGnedin"])):
+    recLabels=rec.dtype.fields.keys() # note, this list is unordered since rec is a dict, so we need to use labels (which should be sorted to match the order of columns in pars array)
+    pars=np.zeros((len(rec),len(labels)))
+    for ii in range(len(labels)):
+        pars[:,ii]=rec[labels[ii]]
+    return pars
+
+def writeRec(rec,filename,clobber=True,compress="GZIP"):
+    fitsio.write(filename,rec,clobber=clobber,compress=compress)
+
+def readRec(filename):
+    rec=fitsio.read(filename)
+    return rec
