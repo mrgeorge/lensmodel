c26668f6ed68aad7a0165364e454912789c0f92d
Matt George <mattgeorge1@gmail.com>
2013-08-13 22:59:43 +0000

    aesthetic tweaks to plot linewidths and colors

--- compare_profiles.py
+++ compare_profiles.py
@@ -28,7 +28,7 @@ dlog10R=np.log10(Rmax/Rmin)/(nBins-1)
 Rkpc=Rmin*10.**(np.arange(nBins) * dlog10R)
 rkpc=Rkpc
 
-nradAC=600
+nradAC=500
 
 rhoNFW=lensmodel.profiles.rhoNFW(rkpc, mhalo, conc, od)
 rhoAC=lensmodel.profiles.rhoAC(rkpc, mhalo, conc, od, 2, innerSlopeGNFW, nuDuttonAC, AGnedin, wGnedin, mstars, rstars, nrad=nradAC)
@@ -46,45 +46,48 @@ plt.clf()
 figsize=(10,5)
 logylim=(1.,1.e3)
 linylim=(0.75,1.09)
-
+lw=3
 
 plt.rc('font',**{'family':'sans-serif','sans-serif':['Helvetica'],'size':12})
 plt.rc('text', usetex=True)
 plt.rc('axes',linewidth=1.5)
 
 fig,axarr=plt.subplots(2,3,figsize=figsize)
-fig.subplots_adjust(hspace=0,wspace=0.5)
+fig.subplots_adjust(hspace=0,wspace=0.45)
 
 plt.sca(axarr[0,0])
 lensmodel.plot.plotProfile(rkpc,rhoAC,xlim=(Rmin,Rmax),xscale="log",yscale="log",ylabel=r"$\rho~($M$_{\odot}~$pc$^{-3})$",label="AC",color="blue",ls="--")
-lensmodel.plot.plotProfile(rkpc,rhoNFW,label="NFW",color="black",ls="-")
-lensmodel.plot.plotProfile(rkpc,rhoExp,label="Expansion",color="green",ls=":")
+lensmodel.plot.plotProfile(rkpc,rhoNFW,label="NFW",color="black",ls="-",lw=1)
+lensmodel.plot.plotProfile(rkpc,rhoExp,label="Expansion",color="red",ls=":")
 plt.setp(axarr[0,0].get_xticklabels(),visible=False)
 plt.legend(loc="lower left", frameon=False, prop={'size':12})
 
 plt.sca(axarr[0,1])
 lensmodel.plot.plotProfile(Rkpc,sigmaAC,xlim=(Rmin,Rmax),ylim=logylim,xscale="log",yscale="log",ylabel=r"$\Sigma~($M$_{\odot}~$pc$^{-2})$",color="blue",ls="--")
-lensmodel.plot.plotProfile(Rkpc,sigmaNFW)
-lensmodel.plot.plotProfile(Rkpc,sigmaExp,color="green",ls=":")
+lensmodel.plot.plotProfile(Rkpc,sigmaNFW,lw=1)
+lensmodel.plot.plotProfile(Rkpc,sigmaExp,color="red",ls=":")
 plt.setp(axarr[0,1].get_xticklabels(),visible=False)
 
 plt.sca(axarr[0,2])
 lensmodel.plot.plotProfile(Rkpc,deltaSigmaAC,xlim=(Rmin,Rmax),ylim=logylim,xscale="log",yscale="log",ylabel=r"$\Delta\Sigma~($M$_{\odot}~$pc$^{-2})$",color="blue",ls="--")
-lensmodel.plot.plotProfile(Rkpc,deltaSigmaNFW)
-lensmodel.plot.plotProfile(Rkpc,deltaSigmaExp,color="green",ls=":")
+lensmodel.plot.plotProfile(Rkpc,deltaSigmaNFW,lw=1)
+lensmodel.plot.plotProfile(Rkpc,deltaSigmaExp,color="red",ls=":")
 plt.setp(axarr[0,2].get_xticklabels(),visible=False)
 
 plt.sca(axarr[1,0])
 lensmodel.plot.plotProfile(rkpc,rhoAC/rhoNFW,xlim=(Rmin,Rmax),ylim=linylim,xscale="log",yscale="linear",ylabel=r"$\rho/\rho_{NFW}$",xlabel=r"$r$ (kpc)",color="blue",ls="--")
-lensmodel.plot.plotProfile(rkpc,rhoExp/rhoNFW,yscale="linear",color="green",ls=":")
+lensmodel.plot.plotProfile(rkpc,rhoNFW/rhoNFW,yscale="linear",color="black",ls="-",lw=1)
+lensmodel.plot.plotProfile(rkpc,rhoExp/rhoNFW,yscale="linear",color="red",ls=":")
 
 plt.sca(axarr[1,1])
 lensmodel.plot.plotProfile(Rkpc,sigmaAC/sigmaNFW,xlim=(Rmin,Rmax),ylim=linylim,xscale="log",yscale="linear",ylabel=r"$\Sigma/\Sigma_{NFW}$",xlabel=r"$R$ (kpc)",color="blue",ls="--")
-lensmodel.plot.plotProfile(rkpc,sigmaExp/sigmaNFW,yscale="linear",color="green",ls=":")
+lensmodel.plot.plotProfile(rkpc,sigmaNFW/sigmaNFW,yscale="linear",color="black",ls="-",lw=1)
+lensmodel.plot.plotProfile(rkpc,sigmaExp/sigmaNFW,yscale="linear",color="red",ls=":")
 
 plt.sca(axarr[1,2])
 lensmodel.plot.plotProfile(Rkpc,deltaSigmaAC/deltaSigmaNFW,xlim=(Rmin,Rmax),ylim=linylim,xscale="log",yscale="linear",ylabel=r"$\Delta\Sigma/\Delta\Sigma_{NFW}$",xlabel=r"$R$ (kpc)",color="blue",ls="--")
-lensmodel.plot.plotProfile(rkpc,deltaSigmaExp/deltaSigmaNFW,yscale="linear",color="green",ls=":")
+lensmodel.plot.plotProfile(rkpc,deltaSigmaNFW/deltaSigmaNFW,yscale="linear",color="black",ls="-",lw=1)
+lensmodel.plot.plotProfile(rkpc,deltaSigmaExp/deltaSigmaNFW,yscale="linear",color="red",ls=":")
 
 
 plt.savefig("../plots/compareAC.pdf")
