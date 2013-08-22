ac0858fd47ca7cf7d74cf59ec023e3ea2cbccbda
Matt George <mattgeorge1@gmail.com>
2013-08-22 04:24:03 +0000

    hacked for loops to run a few of the new target samples: sm9.5,10.5,11.5

--- batchRun.py
+++ batchRun.py
@@ -6,7 +6,7 @@
 import os
 
 survey=["sdss","lsst"]
-target=["galaxy","cluster"]
+target=["galaxy","cluster","sm9.5","sm10.5","sm11.5"]
 Rmin=[20.,40.]
 magFrac=[0.5,1.0]
 concPriorType=["true","low","high","free"]
@@ -19,10 +19,12 @@ elif(queue=="big"):
     nThreads=48
 
 for ss in range(len(survey)):
-    for tt in range(len(target)):
+#    for tt in range(len(target)):
+    for tt in [3,4,2]:
         for rr in range(len(Rmin)):
             for mm in range(len(magFrac)):
-                for cc in range(len(concPriorType)):
+#                for cc in range(len(concPriorType)):
+                for cc in [0,3]:
                     command="python $HOME/sdsslens/lensmodel/driver.py {} {} {} {} {} {}".format(survey[ss],target[tt],Rmin[rr],magFrac[mm],concPriorType[cc],nThreads)
                     jobName="j{}{}{}{}{}".format(ss,tt,rr,mm,cc)
                     os.system("echo {} | qsub -q {} -l nodes=1:ppn={} -j oe -m bea -M mgeorge@astro.berkeley.edu -V -N {}".format(command,queue,nThreads,jobName))
